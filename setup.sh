917f4306eaa789637fe0e8e88f545e6c609d8de4
Coding-Powers <zuhair.alsarhi@std.medipol.edu.tr>
2024-11-06 14:45:10 +0300

    Upload setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+RESET='\033[0m'  # Reset to default color
+
+echo -e "${CYAN}==================== Installing Wordpress ====================${RESET}"
+sudo apt update -y && sudo apt upgrade -y && sudo apt install docker-compose -y
+wget https://raw.githubusercontent.com/Coding-Powers/install-wordpress-docker/refs/heads/main/docker-compose.yml
+mkdir db_data
+mkdir wp_data
+sudo docker-compose kill && sudo docker-compose up -d
+echo -e "${CYAN}==================== Done Installation ====================${RESET}"
